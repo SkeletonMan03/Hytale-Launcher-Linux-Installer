9e54f0f1206e23a33a8b653ecd39b7d1aa02bdce
Lord SkeletonMan <96273359+SkeletonMan03@users.noreply.github.com>
2026-02-19 19:25:35 -0600

    Update Install.sh
    Remove showing DL URL

--- Install.sh
+++ Install.sh
@@ -8,7 +8,6 @@ rm -rf ~/.local/share/Hytale/
 #Now for the actual download
 echo "Downloading and installing Hytale launcher"
 DLURL=$(curl https://launcher.hytale.com/version/release/launcher.json | jq -r '.download_url.linux.amd64.url')
-echo $DLURL
 sudo mkdir -p /opt/hytale/
 sudo chown -R $USER /opt/hytale
 curl $DLURL -o /opt/hytale/hytale-launcher.zip
